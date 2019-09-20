1ff509623bec8bab6a90a3d1b8f56b54d24e3957
Nathan Pickard <nathan.pickard87@gmail.com>
2019-09-20 14:29:26 -0700

    Navigation sidenav and toolbar heading added

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,7 +6,9 @@ import {
   MatNativeDateModule,
   MatFormFieldModule,
   MatIconModule,
-  MatInputModule
+  MatInputModule,
+  MatSidenavModule,
+  MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
@@ -17,7 +19,9 @@ import {
     MatNativeDateModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule
   ],
   exports: [
     MatButtonModule,
@@ -26,7 +30,9 @@ import {
     MatNativeDateModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    MatSidenavModule,
+    MatToolbarModule
   ]
 })
 
